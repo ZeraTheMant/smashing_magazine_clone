f6ac4a02c9080e6eb4e4fcd66a0cc1fdbe418a04
zerathemant <imbapalagz@gmail.com>
2021-04-06 15:57:15 +0800

    navbar near completion, todo: desktop view for more button

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const navBtn = {
 	domElement: document.querySelector("#nav-btn"),
 	timesIcon: document.querySelector("#nav-btn-times-icon"),
+	mobileOptions: document.querySelector("#mobile-options"),
+	moreCat: document.querySelector("#more-cat"),
+	lessCat: document.querySelector("#less-cat"),
 	clicked: false,
 	toggleEvent: function() {
 		if (navBtn.clicked) {
 			navBtn.domElement.firstElementChild.textContent = "More";
 			navBtn.timesIcon.classList.add("hidden");
+			navBtn.mobileOptions.classList.add("hidden");
+			
+			navBtn.lessCat.classList.add("hidden");
+			navBtn.moreCat.classList.remove("hidden");
 		} else {
 			navBtn.domElement.firstElementChild.textContent = "Less";
 			navBtn.timesIcon.classList.remove("hidden");
+			navBtn.mobileOptions.classList.remove("hidden");
+			
+			navBtn.lessCat.classList.remove("hidden");
+			navBtn.moreCat.classList.add("hidden");
 		}
 		
 		navBtn.clicked = !navBtn.clicked;
