2bef8cf2da3115105d47995f24342bd56ef1d697
zerathemant <imbapalagz@gmail.com>
2021-04-08 10:03:22 +0800

    working on mobile options while resizing screen

--- index.js
+++ index.js
@@ -2,21 +2,43 @@ const navBtn = {
 	domElement: document.querySelector("#nav-btn"),
 	timesIcon: document.querySelector("#nav-btn-times-icon"),
 	mobileOptions: document.querySelector("#mobile-options"),
+	desktopOptions: document.querySelector("#desktop-options"),
 	moreCat: document.querySelector("#more-cat"),
 	lessCat: document.querySelector("#less-cat"),
 	clicked: false,
+	screenResizeSetup: function () {
+		if (navBtn.clicked) {
+			if (window.innerWidth > 779) {
+				navBtn.desktopOptions.classList.remove("hidden");
+				navBtn.mobileOptions.classList.add("hidden");
+			} else {
+				navBtn.mobileOptions.classList.remove("hidden");
+				navBtn.desktopOptions.classList.add("hidden");
+			}		
+		}			
+	},
 	toggleEvent: function() {
 		if (navBtn.clicked) {
 			navBtn.domElement.firstElementChild.textContent = "More";
 			navBtn.timesIcon.classList.add("hidden");
-			navBtn.mobileOptions.classList.add("hidden");
+			
+			if (window.innerWidth > 779) {
+				navBtn.desktopOptions.classList.add("hidden");
+			} else {
+				navBtn.mobileOptions.classList.add("hidden");
+			}		
 			
 			navBtn.lessCat.classList.add("hidden");
 			navBtn.moreCat.classList.remove("hidden");
 		} else {
 			navBtn.domElement.firstElementChild.textContent = "Less";
 			navBtn.timesIcon.classList.remove("hidden");
-			navBtn.mobileOptions.classList.remove("hidden");
+			
+			if (window.innerWidth > 779) {
+				navBtn.desktopOptions.classList.remove("hidden");
+			} else {
+				navBtn.mobileOptions.classList.remove("hidden");
+			}			
 			
 			navBtn.lessCat.classList.remove("hidden");
 			navBtn.moreCat.classList.add("hidden");
@@ -27,3 +49,4 @@ const navBtn = {
 }
 
 navBtn.domElement.addEventListener('click', navBtn.toggleEvent);
+window.addEventListener('resize', navBtn.screenResizeSetup);
